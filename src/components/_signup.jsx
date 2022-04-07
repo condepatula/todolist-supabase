560ab948e8747036cc5e5c08ff4d780a6faed717
unknown <lutormig@gmail.com>
2022-04-07 16:05:51 -0400

    Refactor signup

--- src/components/_signup.jsx
+++ src/components/_signup.jsx
@@ -1,43 +1,57 @@
 import {
-  Input,
   Button,
   Text,
-  InputWrapper,
   Box,
   Group,
   Divider,
   Image,
+  TextInput,
+  PasswordInput,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { showNotification } from "@mantine/notifications";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../api/client";
 import { useTodolist } from "../context/todolist-context";
-import { At, Check, X } from "tabler-icons-react";
+import { Check, X, EyeCheck, EyeOff } from "tabler-icons-react";
 import signupLogo from "../assets/img/signup.png";
 
 export const Signup = () => {
   const navigate = useNavigate();
   const { setLoggedIn, setUser } = useTodolist();
   const [loading, setLoading] = useState(false);
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const signup = async (e) => {
-    e.preventDefault();
+  const form = useForm({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validate: {
+      username: (value) =>
+        value.length < 10 ? "Must be 10 or more characters" : null,
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      confirmPassword: (value, values) =>
+        value !== values.password ? "Passwords did not match" : null,
+    },
+  });
+
+  const signUp = async (data) => {
     try {
       setLoading(true);
-      const { user, error } = await supabase.auth.signUp({ email, password });
+      const { user, error } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password,
+      });
       if (error) throw error;
       showNotification({
         message: "Account has created!",
         icon: <Check />,
         color: "teal",
       });
-      console.log(user);
-      createProfile(user.id);
+      createProfile(user.id, data.username, data.email);
       setLoggedIn(true);
       navigate("/");
     } catch (error) {
@@ -51,14 +65,13 @@ export const Signup = () => {
     }
   };
 
-  const createProfile = async (id) => {
+  const createProfile = async (id, username, email) => {
     try {
       const { data, error } = await supabase
         .from("profiles")
         .insert({ id, updated_at: new Date(), username })
         .single();
       if (error) throw error;
-      console.log(data);
       setUser({ id: data.id, name: data.username, email });
     } catch (error) {
       console.log(error);
@@ -69,52 +82,51 @@ export const Signup = () => {
 
   return (
     <Box sx={{ height: "100vh" }}>
-      <Group spacing={5}>
+      <Group sx={{ display: "flex", alignItems: "center" }} spacing={5}>
         <Image src={signupLogo} width={40} />
-        <Text mt={10} weight={500} size="xl">
-          Signup
+        <Text weight={500} size="xl">
+          Sign up
         </Text>
       </Group>
-      <Divider mb={50} />
-      <Box sx={{ maxWidth: "500px", margin: "auto" }}>
-        <form onSubmit={(e) => signup(e)}>
-          <InputWrapper id="username" label="User name" required mb={10}>
-            <Input
-              id="username"
-              placeholder="Your user name"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </InputWrapper>
-          <InputWrapper id="email" label="Email" required mb={10}>
-            <Input
-              id="email"
-              icon={<At />}
-              placeholder="Your email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </InputWrapper>
-          <InputWrapper id="password" label="Password" required mb={10}>
-            <Input
-              id="password"
-              placeholder="Your password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </InputWrapper>
-          <InputWrapper id="confirm-password" label="Confirm Password" required>
-            <Input
-              id="confirm-password"
-              placeholder="Confirm your password"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </InputWrapper>
+      <Divider mb="xl" mt="sm" />
+      <Box sx={{ maxWidth: "500px", margin: "auto" }} pl={10} pr={10}>
+        <form onSubmit={form.onSubmit((values) => signUp(values))}>
+          <TextInput
+            required
+            description="Username must be 10 or more characters"
+            label="Username"
+            placeholder="Your username"
+            {...form.getInputProps("username")}
+          />
+          <TextInput
+            mt="sm"
+            required
+            label="Email"
+            placeholder="your@email.com"
+            {...form.getInputProps("email")}
+          />
+          <PasswordInput
+            mt="sm"
+            required
+            label="Password"
+            placeholder="Password"
+            visibilityToggleIcon={({ reveal, size }) =>
+              reveal ? <EyeOff size={size} /> : <EyeCheck size={size} />
+            }
+            {...form.getInputProps("password")}
+          />
+
+          <PasswordInput
+            mt="sm"
+            required
+            label="Confirm password"
+            placeholder="Confirm password"
+            visibilityToggleIcon={({ reveal, size }) =>
+              reveal ? <EyeOff size={size} /> : <EyeCheck size={size} />
+            }
+            {...form.getInputProps("confirmPassword")}
+          />
+
           <Button
             fullWidth
             mt={30}
@@ -123,7 +135,7 @@ export const Signup = () => {
             type="submit"
             loading={loading}
           >
-            {loading ? "Submiting..." : "Submit"}
+            {loading ? "Registering..." : "Register"}
           </Button>
         </form>
       </Box>
